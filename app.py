6e480d0ba69657dc6e94a2eecde75cc1ce1dde2e
ys <50498518+irissong49@users.noreply.github.com>
2024-04-26 13:56:10 +0800

    initial stuff
    should work locally

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+received_message = "initial placeholder"  # 用于存储服务器端收到的消息
+
+@app.route('/data')
+def get_data():
+    # 在这里获取数据，这里简单地返回一个字典作为示例
+    return received_message
+
+@app.route('/')
+def index():
+    return render_template('index.html', message=received_message)
+
+@app.route('/send', methods=['POST'])
+def send_message():
+    global sent_message
+    sent_message = request.data.decode('utf-8')  # 将接收到的数据解码为字符串
+    return sent_message
+
+@app.route('/receive', methods=['POST'])
+def receive_message():
+    global received_message
+    received_message = request.data.decode('utf-8')  # 将接收到的数据解码为字符串\
+    print("received"+ received_message)
+    #return jsonify({'script': 'updateReceivedMsg(' + received_message + ');'})
+    #return 'Message received successfully! from receive_message888'
+    return jsonify({'script': 'test();'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
