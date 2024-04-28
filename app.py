c66a721e8a095b8b6b448c83c63accd305b5e395
ys <50498518+irissong49@users.noreply.github.com>
2024-04-29 00:03:47 +0800

    restrict input before receiving response

--- app.py
+++ app.py
@@ -11,13 +11,15 @@ f.close()
 
 WEBPORT=config['websitePort']
 
+input_restriction= False
 received_message = "initial placeholder"  # 用于存储服务器端收到的消息
 sent_message = "initial placeholder"
 
 @app.route('/data')
 def get_data():
     # 在这里获取数据，这里简单地返回一个字典作为示例
-    return jsonify({'received_message':received_message, 'sent_message':sent_message})
+
+    return jsonify({'received_message':received_message, 'sent_message':sent_message,'input_restriction':input_restriction})
 
 @app.route('/')
 def index():
@@ -26,7 +28,10 @@ def index():
 @app.route('/send', methods=['POST'])
 def send_message():
     global sent_message
+    global input_restriction
     sent_message = request.data.decode('utf-8')  # 将接收到的数据解码为字符串
+
+    input_restriction=True
     return sent_message
 
 @app.route('/whatareyousaying', methods=['POST'])
@@ -36,11 +41,13 @@ def receive_query():
 @app.route('/receive', methods=['POST'])
 def receive_message():
     global received_message
+    global input_restriction
     received_message = request.data.decode('utf-8')  # 将接收到的数据解码为字符串\
     print("received"+ received_message)
+    input_restriction=False
     #return jsonify({'script': 'updateReceivedMsg(' + received_message + ');'})
     #return 'Message received successfully! from receive_message888'
-    return jsonify({'script': 'test();'})
+    return "message received by website, thanks"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=WEBPORT)
